8f44f0a10e86f2c0e9507d0d12896d9d060b3f0a
redouane <r80@live.fr>
2014-09-25 23:32:05 +0100

    Update and rename meoinit.cpp to egamiinit.cpp

--- egamiinit.cpp
+++ egamiinit.cpp
@@ -32,11 +32,11 @@ int TestMount(std::string dev)
 {
 	std::string cmd;
 	int rc;
-	system("mkdir /media/meoboot");
-	cmd = "mount " + dev + " /media/meoboot > /dev/null 2>&1";
+	system("mkdir /media/egamiboot");
+	cmd = "mount " + dev + " /media/egamiboot > /dev/null 2>&1";
 	rc = system(cmd.c_str());
-	if (access("/media/meoboot/MbootM/.meoboot", F_OK) != 0) {
-		cmd = "umount " + dev + " /media/meoboot > /dev/null 2>&1";
+	if (access("/media/egamiboot/EgamiBootI/.egamiboot", F_OK) != 0) {
+		cmd = "umount " + dev + " /media/egamiboot > /dev/null 2>&1";
 		rc = system(cmd.c_str());
 		return 0;
 	} else {
@@ -49,7 +49,7 @@ int main(int argc, char **argv)
 	char buf[256];
 	int check = 0;
 	int checkgo = 0;
-	std::string MEOHOME = "/media/meoboot";
+	std::string EGAMIHOME = "/media/egamiboot";
 	std::string TARGET = "Flash";
 	std::string tmpstr = "";
 	std::string tmpstr2 = "";
@@ -92,7 +92,7 @@ if (checkgo == 1) {
 	check = system("umount /media/sdc1");
 	check = system("umount /media/sdd1");
 	sleep(2);
-	check = system("/usr/bin/showiframe /usr/lib/enigma2/python/Plugins/Extensions/MeoBoot/icons/meoboot_i.mvi");
+	check = system("/usr/bin/showiframe /usr/lib/enigma2/python/Plugins/Extensions/EGAMIBoot/egamiboot.mvi");
 	
 	DIR *d = opendir("/dev/");
 	if (d)
@@ -114,11 +114,11 @@ if (checkgo == 1) {
 	sleep(1);
 //	system ("cat /proc/stb/avs/0/colorformat > /proc/stb/avs/0/colorformat");
 // bootmanager
-	if (access("/usr/lib/enigma2/python/Plugins/Extensions/MeoBoot/contrib/meobm", F_OK) == 0)
-		check = system("/usr/lib/enigma2/python/Plugins/Extensions/MeoBoot/contrib/meobm");
+	if (access("/usr/lib/enigma2/python/Plugins/Extensions/EGAMIBoot/bin/egamibm", F_OK) == 0)
+		check = system("/usr/lib/enigma2/python/Plugins/Extensions/EGAMIBoot/bin/egamibm");
 	
 	
-	f = fopen("/media/meoboot/MbootM/.meoboot", "rt");
+	f = fopen("/media/egamiboot/EgamiBootI/.egamiboot", "rt");
   	 if (f) {
 		fgets(buf, 256, f);
 		TARGET = string(buf);
@@ -133,47 +133,47 @@ if (checkgo == 1) {
 			check = system("/usr/bin/showiframe /usr/share/bootlogo.mvi > /dev/null 2>&1");
 		//sleep(1);
 	} else {
-		tmpstr = "/media/meoboot/MbootM/" + TARGET;
+		tmpstr = "/media/egamiboot/EgamiBootI/" + TARGET;
 		if (DirectoryExists(tmpstr.c_str())) {
-			tmpstr = "/media/meoboot/MbootM/" + TARGET + "/usr/share/bootlogo.mvi";
+			tmpstr = "/media/egamiboot/EgamiBootI/" + TARGET + "/usr/share/bootlogo.mvi";
 			if (access(tmpstr.c_str(), F_OK) == 0)
 				tmpstr = "/usr/bin/showiframe " + tmpstr;
 			else
-				tmpstr = "/usr/bin/showiframe /media/meoboot/MbootM/" + TARGET + "/boot/bootlogo.mvi";
+				tmpstr = "/usr/bin/showiframe /media/egamiboot/EgamiBootI/" + TARGET + "/boot/bootlogo.mvi";
 			check = system(tmpstr.c_str());
 			//sleep(1);
 	
-			tmpstr = "/media/meoboot/MbootM/" + TARGET + "/dev";
+			tmpstr = "/media/egamiboot/EgamiBootI/" + TARGET + "/dev";
 			tmpstr2 = "/bin/mount -o bind /dev " + tmpstr;
 			check = system(tmpstr2.c_str());
 
-			tmpstr = "/media/meoboot/MbootM/" + TARGET + "/proc";
+			tmpstr = "/media/egamiboot/EgamiBootI/" + TARGET + "/proc";
 			tmpstr2 = "/bin/mount -o bind /proc " + tmpstr;
 			check = system(tmpstr2.c_str());
 			
-			tmpstr = "/media/meoboot/MbootM/" + TARGET + "/sys";
+			tmpstr = "/media/egamiboot/EgamiBootI/" + TARGET + "/sys";
 			tmpstr2 = "/bin/mount -o bind /sys " + tmpstr;
 			check = system(tmpstr2.c_str());
 
-			tmpstr = "/media/meoboot/MbootM/" + TARGET + "/media/meoboot";
+			tmpstr = "/media/egamiboot/EgamiBootI/" + TARGET + "/media/egamiboot";
 			tmpstr2 = "rm -r" + tmpstr + " > /dev/null 2>&1";
 			check = system(tmpstr2.c_str());
 			
-			tmpstr = "/media/meoboot/MbootM/" + TARGET + "/media/meoboot";
+			tmpstr = "/media/egamiboot/EgamiBootI/" + TARGET + "/media/egamiboot";
 			tmpstr2 = "mkdir " + tmpstr + " > /dev/null 2>&1";
 			check = system(tmpstr2.c_str());
 /*
-			tmpstr = "/media/meoboot /media/meoboot/MbootM/" + TARGET + "/meoboot";
+			tmpstr = "/media/egamiboot /media/egamiboot/EgamiBootI/" + TARGET + "/egamiboot";
 			tmpstr2 = "/bin/mount -o bind " + tmpstr;
 			check = system(tmpstr2.c_str());
 
-			tmpstr2 = "rmdir /media/meoboot/MbootM/" + TARGET + "/media/meoboot";
+			tmpstr2 = "rmdir /media/egamiboot/EgamiBootI/" + TARGET + "/media/egamiboot";
 			check = system(tmpstr2.c_str());
 
-			tmpstr2 = "rm /media/meoboot/MbootM/" + TARGET + "/media/meoboot";
+			tmpstr2 = "rm /media/egamiboot/EgamiBootI/" + TARGET + "/media/egamiboot";
 			check = system(tmpstr2.c_str());
 			
-			tmpstr2 = "rm /media/meoboot/MbootM/" + TARGET + mydir;
+			tmpstr2 = "rm /media/egamiboot/EgamiBootI/" + TARGET + mydir;
 			check = system(tmpstr2.c_str());
 */
 			d = opendir("/media/");
@@ -181,10 +181,10 @@ if (checkgo == 1) {
 				{
 					while (struct dirent *e = readdir(d))
 					{
-						if (strcmp(e->d_name, "meoboot"))
+						if (strcmp(e->d_name, "egamiboot"))
 						{
 							mydir = "/media/" + std::string(e->d_name);
-							tmpstr = mydir + "/MbootM/.meoboot";
+							tmpstr = mydir + "/EgamiBootI/.egamiboot";
 							if (access(tmpstr.c_str(), F_OK) == 0) 
 							{
 								break;
@@ -195,12 +195,12 @@ if (checkgo == 1) {
 				}
 
 			check = 0;
-			tmpstr = "/media/meoboot/MbootM/" + TARGET + "/etc/init.d/bootmisc.sh";
+			tmpstr = "/media/egamiboot/EgamiBootI/" + TARGET + "/etc/init.d/bootmisc.sh";
 			f = fopen(tmpstr.c_str(), "r");
 			if (f) 
 			{
 				while (fgets(buf, 256, f)) {
-					if (strstr(buf, "meoboot"))
+					if (strstr(buf, "egamiboot"))
 						check = 1;
 				}
 					fclose(f);
@@ -208,9 +208,9 @@ if (checkgo == 1) {
 			
 			if (check == 0) 
 			{
-				tmpstr = "/media/meoboot/MbootM/" + TARGET + "/etc/init.d/bootmisc.sh";
+				tmpstr = "/media/egamiboot/EgamiBootI/" + TARGET + "/etc/init.d/bootmisc.sh";
 				f = fopen(tmpstr.c_str(), "r");
-				tmpstr = "/media/meoboot/MbootM/" + TARGET + "/etc/init.d/bootmisc.tmp";
+				tmpstr = "/media/egamiboot/EgamiBootI/" + TARGET + "/etc/init.d/bootmisc.tmp";
 				FILE *f1 = fopen(tmpstr.c_str(), "w+");
 				if (f && f1) 
 				{
@@ -222,17 +222,17 @@ if (checkgo == 1) {
 						}
 						else
 						{							
-							tmpstr = "if [ ! -d \"/media/meoboot\" ]\; then\n";
-							tmpstr += "	mkdir /media/meoboot\n";
+							tmpstr = "if [ ! -d \"/media/egamiboot\" ]\; then\n";
+							tmpstr += "	mkdir /media/egamiboot\n";
 							tmpstr += "fi\n";
 							tmpstr += "DEVICES1=`find /dev/sd??`\n";
 							tmpstr += "for DEVICE in $DEVICES1;\n";
 							tmpstr += "	do\n";
-							tmpstr += "	if [ ! -e /media/meoboot/MbootM/.meoboot  ]\; then\n";
-							tmpstr += "		mount $DEVICE /media/meoboot\n";
+							tmpstr += "	if [ ! -e /media/egamiboot/EgamiBootI/.egamiboot  ]\; then\n";
+							tmpstr += "		mount $DEVICE /media/egamiboot\n";
 							tmpstr += "	fi\n";
-							tmpstr += "	if [ ! -e /media/meoboot/MbootM/.meoboot  ]\; then\n";
-							tmpstr += "		umount /media/meoboot\n";
+							tmpstr += "	if [ ! -e /media/egamiboot/EgamiBootI/.egamiboot  ]\; then\n";
+							tmpstr += "		umount /media/egamiboot\n";
 							tmpstr += "	else\n";
 							tmpstr += "		break\n";
 							tmpstr += "	fi\n";
@@ -250,14 +250,14 @@ if (checkgo == 1) {
 							
 					fclose(f);
 					fclose(f1);
-					tmpstr = "mv -f /media/meoboot/MbootM/" + TARGET + "/etc/init.d/bootmisc.tmp /media/meoboot/MbootM/" + TARGET + "/etc/init.d/bootmisc.sh";
+					tmpstr = "mv -f /media/egamiboot/EgamiBootI/" + TARGET + "/etc/init.d/bootmisc.tmp /media/egamiboot/EgamiBootI/" + TARGET + "/etc/init.d/bootmisc.sh";
 					check = system(tmpstr.c_str());
-					tmpstr = "chmod 0775 /media/meoboot/MbootM/" + TARGET + "/etc/init.d/bootmisc.sh";
+					tmpstr = "chmod 0775 /media/egamiboot/EgamiBootI/" + TARGET + "/etc/init.d/bootmisc.sh";
 					check = system(tmpstr.c_str());
 				}
 			}
 		
-			tmpstr = "/media/meoboot/MbootM/" + TARGET;
+			tmpstr = "/media/egamiboot/EgamiBootI/" + TARGET;
 			chdir (tmpstr.c_str());
 			chroot(".");
 		}
